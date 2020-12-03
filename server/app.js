89691e4f0278bd75ecb11ef49480d1a301d1b0e5
Christian Werner <ccwerner96@gmail.com>
2020-12-02 21:18:19 -0300

    refactor: updating package.json

--- server/app.js
+++ server/app.js
@@ -1,36 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cookieParser from "cookie-parser";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import usersRouter from "./routes/users.js";
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+console.log(process.env.PORT);
+const PORT = process.env.PORT || 3072;
+
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
