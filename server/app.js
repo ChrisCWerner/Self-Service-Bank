0777969b8ac561e4a5ef23dc0a9e923c9e283b89
Christian Werner <ccwerner96@gmail.com>
2020-12-05 12:08:53 -0300

    chore: added cors to server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
 
@@ -7,11 +8,11 @@ import routes from "./routes/index.js";
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(routes);
 
-console.log(process.env.PORT);
 const PORT = process.env.PORT || 3072;
 
 app.listen(PORT, () => {
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,8 @@ import transactionRoutes from "./transactions.js";
 
 router.use("/users", usersRouter);
 router.use("/transactions", transactionRoutes);
+router.use("/*", (req, res) => {
+  res.status(404).send();
+});
 
 export default router;
